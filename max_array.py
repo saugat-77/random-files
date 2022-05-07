c70fb59563a7d2648b2b5a50273b446f0d708ec1
saugat acharya <59968314+saugat-77@users.noreply.github.com>
2022-05-07 12:51:57 +0545

    Update max_array.py

--- max_array.py
+++ max_array.py
@@ -4,26 +4,6 @@ for i in range(total_number):
     inp_num=int(input('enter array element: '))
     inp_arr += [inp_num]
 
-# inp_arr=[1,2,3,3,7,7,7,7,5,7,5]
-
-# inp_arr.sort()
-# # print(inp_arr)
-
-# x=len(inp_arr)
-# for i in range(x):
-#     y=(inp_arr[x-1])
-
-#     # if i=0:
-#         # 
-
-#     if inp_arr[x] != y: 
-#         print('largest number' +y)
-#         break
-#     x-=1
-
-# inp_arr = [1,2,3,3,5,5,7,7]
-# print(inp_arr)
-
 seen = set()
 duplicate_array = []
 
